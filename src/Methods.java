23c56df1061556d8c251752b5c49c6ca8597d029
Ваше Имя <you@example.com>
2021-08-12 00:01:29 +0300

    Методы.

---
+++ src/Methods.java
@@ -0,0 +1,6 @@
+public class Methods {
+    public static void main (String [] args ) {
+        int result = (int)(Math.random() * 100 + 1);
+        System.out.println(String.format("Случайное число: %s попробуйте еще раз", result));
+    }
+}
